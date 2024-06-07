c4ffee71fe8aaadb21e63d20329128fa3e97783e
Patel Jaivik <jaivik.patel@fabaf.in>
2024-06-07 10:08:00 +0530

    add dynamic data

--- JavaScript/nectr.js
+++ JavaScript/nectr.js
@@ -1,15 +1,32 @@
-$("#smsContent").hide();
+// $("#smsContent").hide();
 
-$("#medicalBtn").click(function () {
-  $(this).addClass("selected");
-  $("#smsBtn").removeClass("selected");
-  $("#smsContent").hide();
-  $("#medicalContent").show();
-});
+// $("#medicalBtn").click(function () {
+//   $(this).addClass("selected");
+//   $("#smsBtn").removeClass("selected");
+//   $("#smsContent").hide();
+//   $("#medicalContent").show();
+// });
 
-$("#smsBtn").click(function () {
-  $(this).addClass("selected");
-  $("#medicalBtn").removeClass("selected");
-  $("#medicalContent").hide();
-  $("#smsContent").show();
+// $("#smsBtn").click(function () {
+//   $(this).addClass("selected");
+//   $("#medicalBtn").removeClass("selected");
+//   $("#medicalContent").hide();
+//   $("#smsContent").show();
+// });
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("smsContent").style.display = "none";
+
+  document.getElementById("medicalBtn").addEventListener("click", function () {
+    this.classList.add("selected");
+    document.getElementById("smsBtn").classList.remove("selected");
+    document.getElementById("smsContent").style.display = "none";
+    document.getElementById("medicalContent").style.display = "flex";
+  });
+
+  document.getElementById("smsBtn").addEventListener("click", function () {
+    this.classList.add("selected");
+    document.getElementById("medicalBtn").classList.remove("selected");
+    document.getElementById("medicalContent").style.display = "none";
+    document.getElementById("smsContent").style.display = "flex";
+  });
 });
--- JavaScript/sugerloafspring.js
+++ JavaScript/sugerloafspring.js
@@ -1,22 +1,46 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+//   // Show medical container by default
+//   //   $("#medicalContainer").show();
+//   $("#smsContainer").hide();
+//   $("#medicalButton").addClass("selected");
+
+//   // Handle click event for medical button
+//   $("#medicalButton").click(function () {
+//     $(this).addClass("selected");
+//     $("#smsButton").removeClass("selected");
+//     $("#medicalContainer").show();
+//     $("#smsContainer").hide();
+//   });
+
+//   // Handle click event for SMS button
+//   $("#smsButton").click(function () {
+//     $(this).addClass("selected");
+//     $("#medicalButton").removeClass("selected");
+//     $("#medicalContainer").hide();
+//     $("#smsContainer").show();
+//   });
+// });
+document.addEventListener("DOMContentLoaded", function () {
   // Show medical container by default
-  //   $("#medicalContainer").show();
-  $("#smsContainer").hide();
-  $("#medicalButton").addClass("selected");
+  //   document.getElementById("medicalContainer").style.display = "block";
+  document.getElementById("smsContainer").style.display = "none";
+  document.getElementById("medicalButton").classList.add("selected");
 
   // Handle click event for medical button
-  $("#medicalButton").click(function () {
-    $(this).addClass("selected");
-    $("#smsButton").removeClass("selected");
-    $("#medicalContainer").show();
-    $("#smsContainer").hide();
-  });
+  document
+    .getElementById("medicalButton")
+    .addEventListener("click", function () {
+      this.classList.add("selected");
+      document.getElementById("smsButton").classList.remove("selected");
+      document.getElementById("medicalContainer").style.display = "flex";
+      document.getElementById("smsContainer").style.display = "none";
+    });
 
   // Handle click event for SMS button
-  $("#smsButton").click(function () {
-    $(this).addClass("selected");
-    $("#medicalButton").removeClass("selected");
-    $("#medicalContainer").hide();
-    $("#smsContainer").show();
+  document.getElementById("smsButton").addEventListener("click", function () {
+    this.classList.add("selected");
+    document.getElementById("medicalButton").classList.remove("selected");
+    document.getElementById("medicalContainer").style.display = "none";
+    document.getElementById("smsContainer").style.display = "flex";
   });
 });
