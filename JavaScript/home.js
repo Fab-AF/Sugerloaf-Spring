ca8a03e14350a6f6d0086396968537cb52b71fd6
Patel Jaivik <jaivik.patel@fabaf.in>
2024-06-12 11:09:15 +0530

    sugerloaf staticcode

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,27 +1,3 @@
-AOS.init();
-
-// You can also pass an optional settings object
-// below listed default settings
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: "aos-init", // class applied after initialization
-  animatedClassName: "aos-animate", // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 400, // values from 0 to 3000, with step 50ms
-  easing: "ease", // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
-});
 document.addEventListener("DOMContentLoaded", function () {
   // Get references to elements
   const nectrContainer = document.getElementById("nectr");
@@ -56,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
     menuItem.style.textDecoration = "none";
     menuItem.style.borderBottom = "1px solid #000"; // Adjust color and thickness as needed
     menuItem.style.borderRadius = "10rem"; // Adjust border radius as needed
-    menuItem.style.padding = "0.7rem 1rem";
+    menuItem.style.padding = "1rem 1rem";
   }
 
   // Set underline for default selected menu item (Nectr)
